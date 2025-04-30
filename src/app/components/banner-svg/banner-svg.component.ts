abda8b3a9a3be67733512b9883d1ad17e17f652f
Cora <enero.noventaytres@gmail.com>
2025-05-01 00:56:16 +0200

    Svg funciona | falta mas capas | no funciona el color con variables

--- src/app/components/banner-svg/banner-svg.component.ts
+++ src/app/components/banner-svg/banner-svg.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'banner-svg',
   imports: [],
   templateUrl: './banner-svg.component.html',
-  styleUrl: './banner-svg.component.css'
-})
-export class BannerSvgComponent {}
+  styleUrl: './banner-svg.component.css',})
+
+export class BannerSvgComponent{} 
