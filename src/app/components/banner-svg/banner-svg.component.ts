1075f4f998e5e60c14899c89829cbb0a3cffe967
Cora <enero.noventaytres@gmail.com>
2025-04-30 11:06:36 +0200

    initial commit | creacion de visualizacion de carpetas

---
+++ src/app/components/banner-svg/banner-svg.component.ts
@@ -0,0 +1,9 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-svg',
+  imports: [],
+  templateUrl: './banner-svg.component.html',
+  styleUrl: './banner-svg.component.css'
+})
+export class BannerSvgComponent {}
