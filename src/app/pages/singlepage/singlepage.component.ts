d74cf991e28823b045a3c9081efcf36ade139248
Cora <enero.noventaytres@gmail.com>
2025-05-02 20:04:49 +0200

    delete component y puesto en la single page

--- src/app/pages/singlepage/singlepage.component.ts
+++ src/app/pages/singlepage/singlepage.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { BannerSvgComponent } from "../../components/banner-svg/banner-svg.component";
-import { NameComponent } from "../../components/name/name.component";
 
 @Component({
   selector: 'app-singlepage',
-  imports: [BannerSvgComponent, NameComponent],
+  imports: [BannerSvgComponent],
   templateUrl: './singlepage.component.html',
   styleUrl: './singlepage.component.css'
 })
 export class SinglepageComponent {
 
+  ngOnInit(): void {
+    document.querySelectorAll('.button').forEach(button => {
+      const icon = button.querySelector('i');
+      const textNode = button.childNodes[button.childNodes.length - 1];
+      if (!textNode || textNode.nodeType !== Node.TEXT_NODE) return;
+  
+      const originalText = textNode.textContent?.trim() || '';
+      const div = document.createElement('div');
+      const letters = originalText.split('');
+  
+      letters.forEach((letter, index, array) => {
+        const span = document.createElement('span');
+        const part = index >= array.length / 2 ? -1 : 1;
+        const position = index >= array.length / 2
+          ? array.length / 2 - index + (array.length / 2 - 1)
+          : index;
+        const move = position / (array.length / 2);
+        const rotate = 1 - move;
+  
+        span.innerHTML = letter === ' ' ? '&nbsp;' : letter;
+        span.style.setProperty('--move', move.toString());
+        span.style.setProperty('--rotate', rotate.toString());
+        span.style.setProperty('--part', part.toString());
+        div.appendChild(span);
+      });
+  
+      div.classList.add('content');
+      button.innerHTML = ''; 
+      if (icon) button.appendChild(icon); 
+      button.appendChild(div); 
+      button.addEventListener('mouseenter', () => {
+        button.classList.add('in');
+        div.classList.add('animate');
+      });
+  
+      button.addEventListener('mouseleave', () => {
+        button.classList.remove('in');
+        div.classList.remove('animate');
+      });
+    });
+  }
+  
 }
