b68ee3092073d35a86537a50b230ba2684dd03e9
Cora <enero.noventaytres@gmail.com>
2025-05-02 11:43:33 +0200

    no me agrada del todo pero he perdido mucho tiempo

--- src/app/pages/singlepage/singlepage.component.ts
+++ src/app/pages/singlepage/singlepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerSvgComponent } from "../../components/banner-svg/banner-svg.component";
+import { NameComponent } from "../../components/name/name.component";
 
 @Component({
   selector: 'app-singlepage',
-  imports: [BannerSvgComponent],
+  imports: [BannerSvgComponent, NameComponent],
   templateUrl: './singlepage.component.html',
   styleUrl: './singlepage.component.css'
 })
