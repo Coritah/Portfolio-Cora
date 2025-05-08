9717b890bc17815cb8004ff498882c77c805e4ed
Cora <enero.noventaytres@gmail.com>
2025-05-08 23:55:05 +0200

    url

--- src/app/services/porfolio.service.ts
+++ src/app/services/porfolio.service.ts
@@ -13,6 +13,6 @@ export class PortfolioService {
   private httpClient = inject(HttpClient);
 
  getLastPortfolio(): Promise<Portfolio> {
-    return lastValueFrom(this.httpClient.get<Portfolio>(`${this.apiUrl}`));
+    return lastValueFrom(this.httpClient.get<Portfolio>(`${this.apiUrl}/last`));
   }
 }
